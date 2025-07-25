471a93a6bc69364023b9519f7237cd82583615a6
dsrkreddy81 <dsrkreddy81@gmail.com>
2025-07-25 04:37:10 -0400

    recommend

---
+++ main.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from fastapi import FastAPI
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+df = None
+tfidf_matrix = None
+cosine_sim = None
+indices = None
+
+def fetch_products():
+    engine = create_engine(DATABASE_URL)
+    query = "SELECT * FROM product;"
+    df = pd.read_sql(query, engine)
+    engine.dispose()
+    return df
+
+def initialize_recommendation_model():
+    global df, tfidf_matrix, cosine_sim, indices
+
+    df = fetch_products()
+    
+    text_columns = ['name', 'category', 'description', 'highlights', 'tags']
+    for col in text_columns:
+        df[col] = df[col].fillna('')
+
+    df['combined_text_features'] = df[text_columns].astype(str).apply(lambda row: ' '.join(row), axis=1)
+
+    tfidf = TfidfVectorizer(stop_words='english', min_df=1, max_df=0.8)
+    tfidf_matrix = tfidf.fit_transform(df['combined_text_features'])
+    
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    
+    df = df.drop_duplicates(subset=['id'])
+    indices = pd.Series(df.index, index=df['id'])
+
+initialize_recommendation_model()
+
+@app.get("/recommend")
+def recommend(productid: str, top_n: int = 5):
+    if productid not in indices:
+        return {"error": "Product not found"}
+
+    idx = indices[productid]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:top_n+1]
+
+    results = []
+    for i, _ in sim_scores:
+        row = df.iloc[i]
+        
+        results.append({
+            "productid": row["id"],
+            "name": row["name"],
+            "category": row.get("category", "N/A"),
+            "listingPrice": row["listingPrice"],
+        })
+    return results
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn main:app --reload --port 8000
