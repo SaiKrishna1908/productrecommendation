368d794110544882e4ed9c2df62665b93c7aff39
saikrishna1908 <saikrishna1908@gmail.com>
2025-08-07 12:46:14 -0500

    add docker file

--- main.py
+++ main.py
@@ -111,5 +111,5 @@ def recommend(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
     # uvicorn main:app --reload --port 8000
